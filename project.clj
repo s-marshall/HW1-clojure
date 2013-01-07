e65858a92ae7e6534386999026a325fa6b26f397
S Marshall <smarshall@ieee.org>
2013-01-07 00:18:54 -0500

    second commit

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 
-(defproject ring-tutorial "0.1.0-SNAPSHOT"
+(defproject ring-tutorial "0.1.0"
   :description "The Ring Tutorial Project"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
         [ring "1.1.6"]]
+  :min-lein-version "2.0.0"
   :main ring-tutorial.core)
--- src/ring_tutorial/core.clj
+++ src/ring_tutorial/core.clj
@@ -1,29 +1,9 @@
-(ns ring-tutorial.core (:require ring.adapter.jetty)
-	(:refer ring.adapter.jetty))
+(ns ring-tutorial.core (:use [ring.adapter.jetty :only [run-jetty]]))
 
 (defn handler [request]
  {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello, World!"})
-   
-(run-jetty handler {:port 3000})
+   :body "Hello, world!"})
 
-(comment
-Run from command line: $ lein run
-
-Or comment out run-jetty line and run in REPL
-$ lein repl
-
-=> (require 'ring.adapter.jetty)
-=> (refer 'ring.adapter.jetty)
-=> (require 'hello-world.core)
-=> (refer 'hello-world.core)
-=> (run-jetty handler {:port 3000})
-
-or
-
-=> (use 'ring.adapter.jetty)
-=> (use 'hello-world.core)
-=> (run-jetty handler {:port 3000})
-
-)
+(defn -main [port]
+	(run-jetty handler {:port (Integer. port)}))
